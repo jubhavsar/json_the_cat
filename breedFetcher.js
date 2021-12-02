b39f62aaaed9d66a6bf973ea98fdd7fc8b1fa410
jubhavsar <juhi_bhavsar@yahoo.com>
2021-12-02 17:06:45 -0500

    Refactor code

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,17 +1,19 @@
 const request = require("request");
-const args = process.argv.slice(2);
-request(
-  `https://api.thecatapi.com/v1/breeds/search?q=${args[0]}`,
-  (error, response, body) => {
-    if (error) {
-      return console.log("error:", error); // Print the error if one occurred
+const fetchBreedDescription = function(breedName, callback) {
+  request(
+    `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`,
+    (error, response, body) => {
+      if (error) {
+        callback(error, null);
+      }
+      console.log("statusCode:", response && response.statusCode); // Print the response status code if a response was received
+      const data = JSON.parse(body);
+      if (data[0]) {
+        callback(null, data[0].description);
+      } else {
+        callback("Not found", null);
+      }
     }
-    console.log("statusCode:", response && response.statusCode); // Print the response status code if a response was received
-    const data = JSON.parse(body);
-    if (data[0]) {
-      console.log(data[0].description);
-    } else {
-      console.log(`Breed is not found`);
-    }
-  }
-);
+  );
+};
+module.exports = { fetchBreedDescription };
